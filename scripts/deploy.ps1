b80d664a334270c2ba49dead8b21a7e24ce9259d
Augusto César Bisognin <augustocb@gmail.com>
2026-03-03 21:22:59 -0300

    pipeline de deploy

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,12 +1,17 @@
+param (
+        [Parameter(HelpMessage = "Environment for deploy: dev, stg, prod (dev)")]
+        [ValidateSet("dev", "stg", "prod", "")]
+        [string]$environment = "dev"
+    )
+
 Write-Host Building...
-rm -Recurse .\dist\
+Remove-Item -Recurse .\dist\
 dotnet publish -c Release -r linux-x64 --no-self-contained -o dist
 
 Write-Host Creating package...
 Compress-Archive -Path .\dist\* -DestinationPath .\dist.zip -Force
 
 Write-Host Uploading...
-$environment = "dev"
 $bucketName = "fiap-mechanics-$environment-lambda-artifacts-$(aws sts get-access-key-info --access-key-id $(aws configure get aws_access_key_id) --query Account --output text)"
 $tag = New-Guid
 aws s3 cp .\dist.zip s3://$bucketName/auth/$tag.zip
