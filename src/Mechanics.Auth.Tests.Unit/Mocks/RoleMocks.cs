9faeb0e2f5d2f02b26fb3b8de104c538e64ff2f9
Augusto César Bisognin <augustocb@gmail.com>
2026-03-05 21:48:12 -0300

    testes unitários

---
+++ src/Mechanics.Auth.Tests.Unit/Mocks/RoleMocks.cs
@@ -0,0 +1,45 @@
+﻿using Mechanics.Auth.Infra.Data.Models;
+
+namespace Mechanics.Auth.Tests.Unit.Mocks;
+
+public static class RoleMocks
+{
+    public static IEnumerable<RoleModel> Roles =>
+    [
+        new() { Id = new Guid("2afde195-550b-498e-a63d-7a6d556b25ba"), Name = "ADMINISTRATOR" },
+        new() { Id = new Guid("a1097867-aa3e-416c-8685-190516b62a12"), Name = "ATTENDANT" },
+        new() { Id = new Guid("f6027484-89a4-49f6-a9cb-4d1733c2bab7"), Name = "MECHANIC" },
+        new() { Id = new Guid("f61b4ae9-cc8f-4fda-a39f-f70bb3c0840f"), Name = "CUSTOMER_USER" },
+        new() { Id = new Guid("f31bca41-0895-4af5-976f-ac892f833b1b"), Name = "CUSTOMER_ADMIN" },
+    ];
+
+    public static class Names
+    {
+        /// <summary>
+        ///     Acesso total ao sistema.
+        /// </summary>
+        public const string Administrator = "ADMINISTRATOR";
+
+        /// <summary>
+        ///     Permite cadastrar clientes e veículos e gerar ordens de serviços.
+        /// </summary>
+        public const string Attendant = "ATTENDANT";
+
+        /// <summary>
+        ///     Permite gerenciar ordens de serviços e realizar manutenções.
+        /// </summary>
+        public const string Mechanic = "MECHANIC";
+
+        /// <summary>
+        ///     Cliente comum.
+        ///     Permite consultar ordens de serviço do seu cliente.
+        /// </summary>
+        public const string CustomerUser = "CUSTOMER_USER";
+
+        /// <summary>
+        ///     Cliente administrador.
+        ///     Permite gerenciar outros usuários do mesmo cliente.
+        /// </summary>
+        public const string CustomerAdmin = "CUSTOMER_ADMIN";
+    }
+}
---
+++ src/Mechanics.Auth.Tests.Unit/Tests/AuthAppServiceTests.cs
@@ -0,0 +1,117 @@
+﻿using Mechanics.Auth.Application.Requests;
+using Mechanics.Auth.Application.Response;
+using Mechanics.Auth.Application.Services;
+using Mechanics.Auth.Application.TokenGenerator;
+using Mechanics.Auth.Infra.Data.Models;
+using Mechanics.Auth.Infra.Data.Repositories;
+using Mechanics.Auth.Tests.Unit.Mocks;
+using Moq;
+
+namespace Mechanics.Auth.Tests.Unit.Tests;
+
+[TestClass]
+[TestCategory("AppService")]
+public class AuthAppServiceTests
+{
+    #region efetuar login
+
+    [TestMethod("Efetuar login com usuário válido deve retornar token")]
+    public async Task It_ShouldReturnToken_WithValidCredentials()
+    {
+        var token = Guid.NewGuid().ToString();
+        var tokenHandlerStub = CreateTokenHandlerStub(token);
+        var user = UserMocks.CreateUser("12345678909", "TEST_5eCre+Key");
+        var repository = CreateUserRepository(user);
+        var appService = new AuthAppService(repository.Object, tokenHandlerStub.Object);
+        var request = new LoginRequest
+        {
+            CpfNumber = "12345678909",
+            Password = "TEST_5eCre+Key",
+        };
+
+        var result = await appService.Login(request);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(token, result.AccessToken);
+        Assert.IsNotNull(result.RefreshToken);
+    }
+
+    [TestMethod("Efetuar login com senha incorreta deve retornar null")]
+    public async Task It_ShouldReturnToken_WithInvalidCredentials()
+    {
+        var token = Guid.NewGuid().ToString();
+        var tokenHandlerStub = CreateTokenHandlerStub(token);
+        var user = UserMocks.CreateUser("12345678909", "TEST_5eCre+Key");
+        var repository = CreateUserRepository(user);
+        var appService = new AuthAppService(repository.Object, tokenHandlerStub.Object);
+        var request = new LoginRequest
+        {
+            CpfNumber = "12345678909",
+            Password = "wrong-password",
+        };
+
+        var result = await appService.Login(request);
+
+        Assert.IsNull(result);
+    }
+
+    #endregion
+
+    #region Renovar token
+
+    [TestMethod("Renovar com refresh token válido")]
+    public async Task It_ShouldReturnToken_WithValidRefreshToken()
+    {
+        var userId = new Guid("5bb2ae44-cbc7-44c4-9eda-cfb860b6e2f5");
+        var user = UserMocks.CreateUser(userId, "38446983028", RoleMocks.Names.Mechanic);
+        var token = Guid.NewGuid().ToString();
+        var refreshToken = Guid.NewGuid().ToString();
+        var tokenHandlerStub = CreateTokenHandlerStub(token, refreshToken, userId.ToString(), userId);
+        var repository = CreateUserRepository(user);
+        var appService = new AuthAppService(repository.Object, tokenHandlerStub.Object);
+        var request = new RefreshTokenRequest { RefreshToken = refreshToken };
+
+        var result = await appService.Refresh(request);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(token, result.AccessToken);
+        Assert.IsNotNull(result.RefreshToken);
+    }
+
+    #endregion
+
+    #region Mocks
+
+    private static Mock<IUserRepository> CreateUserRepository(UserModel user)
+    {
+        var userRepository = new Mock<IUserRepository>();
+
+        userRepository.Setup(repository => repository.GetById(user.Id)).ReturnsAsync(user);
+        userRepository.Setup(repository => repository.GetByCpf(user.CpfNumber)).ReturnsAsync(user);
+
+        return userRepository;
+    }
+
+    private static Mock<IJwtTokenHandler> CreateTokenHandlerStub(string? accessToken = null, string? refreshToken = null,
+        string? securityStamp = null, Guid? userId = null)
+    {
+        var tokenHandler = new Mock<IJwtTokenHandler>();
+        tokenHandler.Setup(handler => handler.GetUserId(refreshToken ?? Guid.NewGuid().ToString())).Returns(userId);
+        tokenHandler.Setup(handler => handler.CreateTokenResponse(It.IsAny<UserModel>())).ReturnsAsync(new TokenResponse
+        {
+            AccessToken = accessToken ?? Guid.NewGuid().ToString(),
+            RefreshToken = refreshToken ?? Guid.NewGuid().ToString(),
+            ExpiresIn = 3600,
+            ExpirationDate = new DateTime(2025, 10, 15, 10, 0, 0, DateTimeKind.Utc),
+        });
+        tokenHandler.Setup(handler =>
+                handler.ValidateRefreshToken(refreshToken ?? Guid.NewGuid().ToString(), securityStamp ?? Guid.NewGuid().ToString()))
+            .ReturnsAsync(true);
+        tokenHandler.Setup(handler => handler.GetUserId(refreshToken ?? Guid.NewGuid().ToString()))
+            .Returns(userId ?? Guid.NewGuid());
+
+        return tokenHandler;
+    }
+
+    #endregion
+}
