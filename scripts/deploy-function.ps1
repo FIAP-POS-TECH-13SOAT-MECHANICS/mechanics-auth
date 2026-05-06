bc9cbeb2e290abe065a6ea061f7f93900b21be56
Augusto César Bisognin <augustocb@gmail.com>
2026-05-06 16:36:19 -0300

    seed database no script local de deploy

--- scripts/deploy-function.ps1
+++ scripts/deploy-function.ps1
@@ -1,8 +1,8 @@
 param (
-        [Parameter(HelpMessage = "Environment for deploy: dev, stg, prod (dev)")]
-        [ValidateSet("dev", "stg", "prod", "")]
-        [string]$environment = "dev"
-    )
+    [Parameter(HelpMessage = "Environment for deploy: dev, stg, prod (dev)")]
+    [ValidateSet("dev", "stg", "prod", "")]
+    [string]$environment = "dev"
+)
 
 Write-Host Building...
 Remove-Item -Recurse .\dist\
@@ -12,7 +12,7 @@ Write-Host Creating package...
 Compress-Archive -Path .\dist\* -DestinationPath .\dist.zip -Force
 
 Write-Host Uploading...
-$bucketName = "fiap-mechanics-$environment-lambda-artifacts-$(aws sts get-access-key-info --access-key-id $(aws configure get aws_access_key_id) --query Account --output text)"
+$bucketName = "fiap-mechanics-$environment-lambda-artifacts-$( aws sts get-access-key-info --access-key-id $( aws configure get aws_access_key_id ) --query Account --output text )"
 $tag = New-Guid
 aws s3 cp .\dist.zip s3://$bucketName/auth/$tag.zip
 
@@ -20,5 +20,20 @@ Write-Host Updating function...
 aws lambda update-function-code --function-name "fiap-mechanics-$environment-auth-token" --s3-bucket $bucketName --s3-key auth/$tag.zip | Out-Null
 aws lambda wait function-updated --function-name "fiap-mechanics-$environment-auth-token"
 aws lambda publish-version --function-name "fiap-mechanics-$environment-auth-token" | Out-Null
+
+Write-Host Seeding database...
+$tableName = "auth-$environment-users"
+$users = Get-Content .\seeds\users.json | ConvertFrom-Json
+foreach ($user in $users) {
+    $item = @{}
+    foreach ($prop in $user.PSObject.Properties) {
+        $item[$prop.Name] = @{ "S" = $prop.Value.ToString() }
+    }
+
+    aws dynamodb put-item `
+        --table-name $tableName `
+        --item "$($item | ConvertTo-Json -Compress)" `
+        --condition-expression "attribute_not_exists(id)"
+}
 
 Write-Host -ForegroundColor Green Lambda function deployed.
