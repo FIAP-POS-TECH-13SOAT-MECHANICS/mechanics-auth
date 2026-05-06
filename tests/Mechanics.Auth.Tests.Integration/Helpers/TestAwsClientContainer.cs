a0e7afdc5f98b8c604ad64931d6adf5c9a825b4c
Augusto César Bisognin <augustocb@gmail.com>
2026-05-06 16:39:46 -0300

    correção teste de integração

--- tests/Mechanics.Auth.Tests.Integration/Helpers/TestAwsClientContainer.cs
+++ tests/Mechanics.Auth.Tests.Integration/Helpers/TestAwsClientContainer.cs
@@ -1,4 +1,5 @@
 ﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
 using Testcontainers.LocalStack;
 
@@ -8,8 +9,10 @@ public class TestAwsClientContainer : IAsyncDisposable
 {
     public IContainer Container { get; } = new LocalStackBuilder("localstack/localstack:3")
         .WithName($"testcontainers-aws-{Guid.NewGuid()}")
-        .WithBindMount(Path.Combine(AppContext.BaseDirectory, "scripts", "localstack"), "/etc/localstack/init/ready.d")
-        .WithBindMount(Path.Combine(AppContext.BaseDirectory, "seeds"), "/seeds")
+        .WithResourceMapping(Path.Combine(AppContext.BaseDirectory, "scripts", "localstack"),
+            "/etc/localstack/init/ready.d", 0, 0,
+            UnixFileModes.UserRead | UnixFileModes.UserExecute | UnixFileModes.GroupRead | UnixFileModes.GroupExecute)
+        .WithResourceMapping(Path.Combine(AppContext.BaseDirectory, "seeds"), "/seeds")
         .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Ready."))
         .WithCleanUp(true)
         .Build();
